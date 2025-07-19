36dfb75655047078ab3dfffe3bb64e5dabda32fb
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2025-07-19 05:25:28 -0500

    Fist commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_demo_disco
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_demo_disco/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_demo_disco
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
